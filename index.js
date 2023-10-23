42938258ddef879f52849b025e1c6ff3e0c9bfd6
Pedro Laraburu <pedrolaraburu@gmail.com>
2023-10-23 19:28:21 -0300

    fix: fix text for 'Planejamento'

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function cardFunction(id) {
     switch(idString){
         case '1':
             secondHalfText.innerHTML = 'Planejamento';
-            pSecondHalf.innerHTML = 'Com o Acquario, tenha total controle sobre seus gastos. Gerencie suas finanças de forma eficiente e simplificada, acompanhe despesas e orçamentos com facilidade, e alcance sua estabilidade financeira com confiança.';
+            pSecondHalf.innerHTML = 'Com o Acquario, faça seu planejamento personalizado com base nas suas preferências. Tenha controle total sobre seus objetivos financeiros e crie um plano que se adapta ao seu estilo de vida e metas, tornando suas finanças mais acessíveis e alinhadas com seus sonhos.';
             break;
         case '2':
             secondHalfText.innerHTML = 'Controle de gastos';
